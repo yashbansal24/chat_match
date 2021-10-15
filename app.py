0db93dedc1d74fd91f5f6d494f8256972206a9fd
Yash Bansal <yashbansal97@gmail.com>
2021-10-15 13:34:54 -0700

    first emotion analysis model

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 from flask import Flask
 from datetime import datetime
-import re
 from flask import render_template
+from emotion_analysis import find_emotion
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template(
+        "home.html"
+    )
 
-@app.route("/hello/<name>")
+@app.route("/hello/")
 def hello_there(name = None):
+    chats = [
+        'I am groot',
+        'Hey baby',
+        'I want to date you']
+    resp = find_emotion(chats)
+    percentage = 0
+    positives = list(filter(lambda r: r[0] == True, resp))
+    for _,p in positives:
+        percentage+=p
+    percentage/=len(positives)
+    percentage*=100.0
     return render_template(
         "home.html",
-        name=name,
-        date=datetime.now()
+        percentage=percentage,
+        chats=chats
     )
