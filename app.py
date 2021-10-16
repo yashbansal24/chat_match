4baae26f0e8e1e9bbedaa1f51b44b49186e4381d
Yash Bansal <yashbansal97@gmail.com>
2021-10-16 03:21:29 -0700

    complete compatibilty percentage calculation

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask import jsonify, request
+from flask_cors import CORS, cross_origin
 from datetime import datetime
 from flask import render_template
 from EmotionAnalysisModel.emotion_analysis import find_emotion
 from FlirtationDetection.flirtation_detection import predictFlirtationBonus
+import json
+import math
 
 app = Flask(__name__)
 
@@ -12,25 +16,19 @@ def home():
         "home.html"
     )
 
-@app.route("/hello/")
+@app.route("/hello", methods=["POST"])
+@cross_origin()
 def hello_there(name = None):
-    chats = [
-        'I am groot',
-        'Hey baby',
-        'I want to date you']
+    chats = json.loads(request.form.get('data'))
     resp = find_emotion(chats)
     percentage = 0
     positives = list(filter(lambda r: r[0] == True, resp))
     for _,p in positives:
         percentage+=p
-    percentage/=len(positives)
+    if len(positives) >= 1:
+        percentage/=len(positives)
     flirtBonus = predictFlirtationBonus(chats)
 
     percentage = min( percentage + flirtBonus , 1)
     percentage*=100.0
-    print(percentage)
-    return render_template(
-        "home.html",
-        percentage=percentage,
-        chats=chats
-    )
+    return jsonify({ "percentage" : math.floor(percentage) })
